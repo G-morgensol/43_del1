06bceae19751849217c92c814db751bd70f2cdc9
Gmorgensol <gustavmorgensol@outlook.com>
2020-09-24 15:21:27 +0200

    Changed name on TotalEyes to TotalEyesPlayer1 variable to make naming more consistent.

--- src/spil/spil.java
+++ src/spil/spil.java
@@ -21,16 +21,17 @@ public class spil {
         while (Player1Score<40 || Player2Score <40) {
             int Dice1Player1 = (int)(Math.random()*range) + min;
             int Dice2Player1 = (int)(Math.random()*range) + min;
-            int TotalEyes = Dice1Player1 + Dice2Player1;
-            Player1Score = Player1Score + TotalEyes;
+            int TotalEyesPlayer1 = Dice1Player1 + Dice2Player1;
+            Player1Score = Player1Score + TotalEyesPlayer1;
             int Dice1Player2 = (int)(Math.random()*range) + min;
             int Dice2Player2 = (int)(Math.random()*range) + min;
             int TotalEyesPlayer2 = Dice1Player2 + Dice2Player2;
             Player2Score = Player2Score + TotalEyesPlayer2;
+            /* test af pointsystem
 
             System.out.println("Spiller 1 har " + Player1Score + " point");
             System.out.println("Spiller 2 har " + Player2Score + " point");
-
+            */
         }
 
     }
