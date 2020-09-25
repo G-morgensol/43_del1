3511d21cfca2762b4d9350e5bb796a18a01f084e
Gmorgensol <gustavmorgensol@outlook.com>
2020-09-25 21:46:12 +0200

    RollDicePlayer2() now functional aswell.

--- src/spil/RollDiceClass.java
+++ src/spil/RollDiceClass.java
@@ -11,19 +11,26 @@ public class RollDiceClass {
         return Player1Dice;
 
     }
-    static void RollDicePlayer2() {
+    static int[] RollDicePlayer2() {
+        int[] Player2Dice = new int[2];
         int max = 6;
         int min = 1;
         int range = max - min + 1;
-        int Dice1Player2 = (int)(Math.random()*range) + min;
-        int Dice2Player2 = (int)(Math.random()*range) + min;
-
+        Player2Dice[0] = (int) (Math.random() * range) + min;
+        Player2Dice[1] = (int) (Math.random() * range) + min;
+        return Player2Dice;
     }
 
     public static void main(String[] args) {
         int[] Player1Dice = RollDicePlayer1();
+        int[] Player2Dice = RollDicePlayer2();
+        /* //Test of the 2 Methods.
+        System.out.println("Player 1 Rolls");
         System.out.println("Dice 1: " + Player1Dice[0]);
         System.out.println("Dice 2: " + Player1Dice[1]);
-
+        System.out.println("Player 1 Rolls");
+        System.out.println("Dice 1: " + Player2Dice[0]);
+        System.out.println("Dice 2: " + Player2Dice[1]);
+*/
     }
 }
