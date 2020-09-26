bd075b0c2705480bb2c8b4b27abaff1fd239483d
Gmorgensol <gustavmorgensol@outlook.com>
2020-09-26 14:32:12 +0200

    Began work on Test.class - imported method RollDicePlayer1() from Rolldice.class - Tested and can print out different dice rolls.

--- src/test/test.java
+++ src/test/test.java
@@ -1,7 +1,11 @@
 package test;
 
+import static spil.RollDice.RollDicePlayer1;
+
 public class test {
     public static void main(String[] args) {
-
+        RollDicePlayer1();
+        System.out.println("Dice 1: " + RollDicePlayer1()[0]);
+        System.out.println("Dice 2: " + RollDicePlayer1()[1]);
     }
 }
