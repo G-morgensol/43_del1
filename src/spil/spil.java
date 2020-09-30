a35e93c592f8929d65289ba3e389f8466e15b680
Gmorgensol <gustavmorgensol@outlook.com>
2020-09-30 18:28:03 +0200

    added some comments

--- src/spil/spil.java
+++ src/spil/spil.java
@@ -3,14 +3,18 @@ public class spil {
     public static void main(String[] args) {
         int i = 0;
         while(true) {
+            //Starts Player 1 Turn
             System.out.println("Player 1 turn");
             Players.Player1Turn();
+            //Starts Player 2 Turn
             System.out.println("Player 2 turn");
             Players.Player2Turn();
 
 
-            //In the unlikely case the game isnt over after 100 iterations, break;
+            //In the extremely unlikely case the game is not over after 100 iterations, break;
             i++;
+            //Iteration print is just for testing purposes, remove at final version.
+            System.out.println("Iteration: " + i);
             if (i ==100) {
                 break;
             }
