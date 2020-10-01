b39e22670db0a635f32e262a0ac43c791fa76122
JensHOls <s201729@student.dtu.dk>
2020-10-01 10:56:31 +0200

    work on returnstatement in "ekstraopgave1"

--- src/spil/ekstraopgave1.java
+++ src/spil/ekstraopgave1.java
@@ -4,32 +4,36 @@ public class ekstraopgave1 {
 
     public static int TotalScore1;
 
+    public static int TotalScore2;
+
     public static int[] Player1Turn() {
 
         int[] Player1Return = new int[2];
         Player1Return[0] = TotalScore1;
         Player1Return[1] = 0;
 
-        int x = EyeSum.SumPlayer1();
+        int DiceTotal = EyeSum.SumPlayer1();
 
-        if (x == 2) {
+        if (DiceTotal == 2) {
             TotalScore1 = 0;
-        }
+        }         else TotalScore1 = TotalScore1 + DiceTotal;
 
-        }
+Players.return Player1Return;
+    }
 
     public static int[] Player2Turn() {
-        public static int TotalScore2;
 
         int[] Player2Return = new int[2];
-        Player2Return[0] = TotalScore1;
+        Player2Return[0] = TotalScore2;
         Player2Return[1] = 0;
 
-        int x = EyeSum.SumPlayer2();
+        int DiceTotal = EyeSum.SumPlayer2();
 
-        if (x == 2) {
-            TotalScore1 = 0;
+        if (DiceTotal == 2) {
+            TotalScore2 = 0;
 
-        }
+        }         else TotalScore2 = TotalScore2 + DiceTotal;
 
-        }
+
+        } Return
+    }
